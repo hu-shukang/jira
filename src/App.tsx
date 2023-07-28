5fcb143752d8070dcb5a88133f0388cedcd6882a
HU SHUKANG <hushukang@gmail.com>
2023-07-28 21:38:21 +0900

    ci: add react-query lib

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import { ProjectListScreen } from 'screens/project-list';
 import { LoginScreen } from 'screens/login';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        {/* <ProjectListScreen /> */}
+        <LoginScreen />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
